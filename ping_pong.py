d092155dbf8908869af1b2fb78dd51bc77900251
Meph1stow <134829637+Meph1stow@users.noreply.github.com>
2023-06-05 15:49:05 +0500

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -17,31 +17,69 @@ class Player(GameSprite):
         keys=key.get_pressed()
         if keys[K_UP] and self.rect.y>5:
             self.rect.y-=self.speed
-        if keys[K_DOWN] and self.rect.y<window_height-80:
+        if keys[K_DOWN] and self.rect.y<win_height-80:
             self.rect.y+=self.speed
     def update_r(self):
         keys=key.get_pressed()
         if keys[K_w] and self.rect.y>5:
             self.rect.y-=self.speed
-        if keys[K_s] and self.rect.y<window_height-80:
+        if keys[K_s] and self.rect.y<win_height-80:
             self.rect.y+=self.speed
 
 back = (200,255,255)
 win_width = 600
 win_height = 500
-window = display.set_mode((win_height,win_width))
+window = display.set_mode((win_width,win_height))
 window.fill(back)
 
-game = True
+game = True   
 finish  =False
 clock = time.Clock()
 FPS = 60
+
+rocket1 = Player('rocket.png',30,200,50,150,4)
+rocket2 = Player('rocket.png',520,200,50,150,4)
+ball = GameSprite('ball.png',200,200,50,50,4)
+
+font.init()
+font = font.SysFont(None,35)
+lose1 = font.render('Player 1 lose', True,(180,0,0))
+lose2 = font.render('Player 2 lose', True,(180,0,0))
+
+speed_x = 3
+speed_y = 3
+
+
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-        if finish != True:
-            window.fill(back)
-        display.update()
-        clock.tick(FPS)
+    if finish != True:
+        window.fill(back)
+        rocket1.update_l()
+        rocket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(rocket1,ball) or sprite.collide_rect(rocket2,ball):
+            speed_x *= -1
+            speed_y *= -1
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1,(200,200))
+            game_over = True
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2,(200,200))
+            game_over = True
+        rocket1.reset()
+        rocket2.reset()
+        ball.reset()
+    display.update()
+    clock.tick(FPS)
+
+
+
+
